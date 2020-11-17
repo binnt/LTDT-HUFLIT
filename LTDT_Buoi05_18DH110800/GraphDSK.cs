7219259ddeecdf63b5f13d6fcf4330a06ed1a7c6
binnt <you@example.com>
2020-11-17 21:00:35 +0700

    first commit

---
+++ LTDT_Buoi05_18DH110800/GraphDSK.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTDT_Buoi05_18DH110800
+{
+    class GraphDSK
+    {
+        int n;
+        LinkedList<int>[] v;
+        bool[] visited;     // visited, pre thêm vào chỉ để tham gia giải thuật
+        int[] pre;
+        //Propeties
+        public LinkedList<int>[] V
+        {
+            get { return v; }
+            set { v = value; }
+        }
+        // Contructor
+        public GraphDSK() { }
+        public GraphDSK(int k)
+        {
+            v = new LinkedList<int>[k];
+            visited = new bool[k];
+        }
+        public GraphDSK(LinkedList<int>[] g)
+        {
+            v = g;
+        }
+        // Doc file ra ds ke
+        public void FileToDSke()
+        {
+            StreamReader sr = new StreamReader("../../TextFile/DSK.txt");
+            n = int.Parse(sr.ReadLine());
+            v = new LinkedList<int>[n];
+            visited = new bool[n];
+            pre = new int[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                v[i] = new LinkedList<int>();
+                string st = sr.ReadLine();
+                // Đặt điều kiện dòng đọc ra không trống
+                if (st != "")
+                {
+                    string[] s = st.Split();
+                    for (int j = 0; j < s.Length; j++)
+                    {
+                        int x = int.Parse(s[j]);
+                        v[i].AddLast(x);
+                    }
+                }
+            }
+            sr.Close();
+        }
+        //Xuat do thi ds ke
+        public void Output()
+        {
+            for (int i = 0; i < v.Length; i++)
+            {
+                foreach (int x in v[i])
+                    Console.Write(x + " ");
+                Console.WriteLine();
+            }
+        }
+        // Duyệt đồ thị với đỉnh xuất phát là s
+        public void DFS(int s)
+        {
+            visited[s] = true;
+            Console.Write(s + "--");
+            foreach (int u in V[s])
+            {
+                if (visited[u] == false)
+                {
+                    pre[u] = s;
+                    DFS(u);
+                }
+            }
+        }
+        public void DuyetDFS(int s)
+        {
+            visited = new bool[n];
+
+            for (int i = 0; i < n; i++)
+            {
+                visited[i] = false;
+                pre[i] = -1;
+            }
+
+            if (visited[s] == false)
+            {
+                DFS(s);
+            }
+        }
+        // End - Duyệt DFS
+        Stack<int> stack;
+        // Tìm đường đi từ đỉnh x đến đỉnh y
+        public void DFSDuongdi(int x, int y)
+        {
+            stack = new Stack<int>();
+            int k = y;
+            do
+            {
+                stack.Push(pre[k]);
+                k = pre[k];
+            }
+            while (pre[k] != x);
+            stack.Push(pre[k]);
+        }
+        public void DFSTimDuongdi(int x, int y)
+        {
+            try
+            {
+                visited = new bool[n];
+
+                for (int i = 0; i < n; i++)
+                {
+                    visited[i] = false;
+                    pre[i] = -1;
+                }
+
+                if (visited[x] == false)
+                {
+                    DFS(x);
+                }
+                Console.WriteLine();
+                DFSDuongdi(x, y);
+                while (stack.Count > 0)
+                {
+                    Console.Write(stack.Pop() + "==");
+
+                }
+                Console.Write(y);
+            }
+            catch
+            {
+
+            }
+        }
+    }
+}
---
+++ LTDT_Buoi05_18DH110800/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LTDT_Buoi05_18DH110800
+{
+    class Program
+    {
+        static int Menu()
+        {
+            int chon;
+            Console.WriteLine();
+            Console.WriteLine("     CHON CONG VIEC");
+            Console.WriteLine("  1. Tao do thi tu file");
+            Console.WriteLine("  2. Xuat do thi DSK len man hinh");
+            Console.WriteLine("  3. Duyet do thi DFS");
+            Console.WriteLine("  4. Tim duong di tu x den y");
+            Console.WriteLine("  0. Thoat");
+            Console.Write("     Chon : ");
+            chon = int.Parse(Console.ReadLine());
+            Console.WriteLine("  **************************************");
+
+            return chon;
+        }
+        static void Main(string[] args)
+        {
+            GraphDSK g = new GraphDSK();
+            int chon;
+
+            do
+            {
+                chon = Menu();
+                Console.WriteLine();
+                switch (chon)
+                {
+                    case 1: { g.FileToDSke(); break; }
+                    case 2:
+                        {
+                            Console.WriteLine("Do thi DSK : ");
+                            g.Output();
+                            break;
+                        }
+                    case 3:
+                        {
+                            Console.WriteLine("Duyet do thi DFS tu cac dinh : ");
+                            for (int i = 0; i < g.V.Length; i++)
+                            {
+                                g.DuyetDFS(i);
+                                Console.WriteLine("");
+                            }
+                            break;
+                        }
+                    case 4:
+                        {
+                            Console.WriteLine("Tim duong di tu dinh x den dinh y");
+                            Console.Write("  Nhap dinh x : ");
+                            int x = int.Parse(Console.ReadLine());
+                            Console.Write("  Nhap dinh y : ");
+                            int y = int.Parse(Console.ReadLine());
+                            g.DFSTimDuongdi(x, y);
+                            break;
+                        }
+                }
+            } while (chon != 0);
+        }
+    }
+}
